259ce5751daea2ed196f26853cec03c951bd0b7e
CC11001100 <CC11001100@qq.com>
2023-02-05 00:28:48 +0800

    add CalculateScoreAndDescSort & add doc

--- examples/main.go
+++ examples/main.go
@@ -1,23 +1,25 @@
-package sentence_score
+package main
 
 import (
 	"fmt"
-	"testing"
+	sentence_score "github.com/cryptography-research-lab/go-sentence-score"
 )
 
-func TestCalculateScore(t *testing.T) {
+func main() {
+
+	// 计算单个句子
 	s := "1123thisisverybkjkjbsecurityfghfkeyassdfdfadasdasdasdasdasdasdasdgoodasdasdking"
-	words, score := CalculateScore(s)
+	words, score := sentence_score.CalculateScore(s)
 	fmt.Println(words)
 	fmt.Println(score)
-}
 
-func TestCalculateScoreAndDescSort(t *testing.T) {
+	// 计算多个句子并排序
 	slice := []string{
 		"1123thisisverybkjkjbsecurityfghfkeygoodasdasdking",
 		"1123thisisverybkjkintenetjbsecurityfghfkeygoodasdasdking",
 		"1123thisisverybknamejkjbsecurityfghfkeygoodasdasdking",
 	}
-	sort := CalculateScoreAndDescSort(slice)
+	sort := sentence_score.CalculateScoreAndDescSort(slice)
 	fmt.Println(sort)
+
 }
